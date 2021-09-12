4c7d01d5eef33704135936d0f7350ad98f45d42b
Daniel Ribeiro <academicodan@gmail.com>
2021-09-12 12:17:17 -0300

    first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.16;
+
+contract Election {
+  // Store condidate
+  // Read candidate 
+  // Constructor
+  string public candidate;
+  constructor() public{
+    candidate = "Candidate 1";
+  }
+}
